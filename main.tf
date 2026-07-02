aaf45df892167b1a7712635ee86854a02ac65e36
Alex Bance <Alex.Bance@justice.gov.uk>
2026-07-02 18:03:40 +0100

    fix: specify port

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "azurerm_application_gateway" "ag" {
       name                                = backend_http_settings.value.name
       probe_name                          = backend_http_settings.value.probe_name
       cookie_based_affinity               = backend_http_settings.value.cookie_based_affinity
-      port                                = 80
+      port                                = backend_http_settings.value.backend_protocol == "Http" ? 80 : 443
       protocol                            = backend_http_settings.value.backend_protocol
       request_timeout                     = 30
       pick_host_name_from_backend_address = backend_http_settings.value.pick_host_name_from_backend_address
